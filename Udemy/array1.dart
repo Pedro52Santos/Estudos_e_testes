72900d66555cdc8cf49dad0fd1a1f78fed2fa8b9
Pedro52Santos <51803100+Pedro52Santos@users.noreply.github.com>
2021-08-13 12:26:32 -0400

    Array
    Só o básico de arrays

--- Udemy/array1.dart
+++ Udemy/array1.dart
@@ -1,1 +1,17 @@
-main(List<String> args) {}
+import 'dart:io';
+
+main(List<String> args) {
+  bool condicao = true;
+  var nomes = [];
+  while (condicao) {
+    print('Insira um nome:');
+    String text = stdin.readLineSync().toString();
+    if (text == 'sair') {
+      print('#########Programa finalizado##########');
+      condicao = false;
+    } else {
+      nomes.add(text);
+    }
+  }
+  print(nomes);
+}
