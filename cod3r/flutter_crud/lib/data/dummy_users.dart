0fc667dfde8e8edf4815875a91fc28103db43195
Pedro52Santos <51803100+Pedro52Santos@users.noreply.github.com>
2021-08-26 10:54:39 -0400

    Aulas Cod3r
    até usando-provider-02

---
+++ cod3r/flutter_crud/lib/data/dummy_users.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      id: '',
+      name: 'maria',
+      email: 'maria@email.com',
+      avatarURL:
+          'https://anime.houseart.me/wp-content/uploads/2020/02/soft.-anime-icons.jpg?v=1582057873'),
+  '2': const User(
+      id: '',
+      name: 'josé',
+      email: 'josé@email.com',
+      avatarURL: 'https://pbs.twimg.com/media/EzButWzWQAIC-lA.png'),
+};
---
+++ cod3r/flutter_crud/lib/models/user.dart
@@ -0,0 +1,13 @@
+class User {
+  final String id;
+  final String name;
+  final String email;
+  final String avatarURL;
+
+  const User({
+    required this.id, //Se der erro voltar aqui para testes.
+    required this.name,
+    required this.email,
+    required this.avatarURL,
+  });
+}
